bc9f82e6a1236622426f9369973fa973f7e6b63a
A-Sravanthi <sravanthiavulaaa@gmail.com>
2025-10-31 22:43:46 +0530

    changed the build config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ensures Vercel knows where to look
+  },
+  base: './', // important for Vercel deployment (fixes blank page issues)
 })
